2ff7728fb06e1959bc490198787e78d495a24be6
borisdvlpr <eduffmiranda@hotmail.com>
2026-08-30 19:11:42 +0100

    feat(model): add CategoryType

---
+++ src/main/java/com/pictet/adventurebook/model/type/CategoryType.java
@@ -0,0 +1,12 @@
+package com.pictet.adventurebook.model.type;
+
+public enum CategoryType {
+    ADVENTURE,
+    FANTASY,
+    FICTION,
+    HORROR,
+    MYSTERY,
+    SCIENCE,
+    SCIENCE_FICTION,
+    THRILLER
+}
