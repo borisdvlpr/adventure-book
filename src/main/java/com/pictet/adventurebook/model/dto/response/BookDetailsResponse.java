9dbbe012b4852c3bf9694cca0409ff5578f5ebe8
borisdvlpr <eduffmiranda@hotmail.com>
2026-09-02 17:46:51 +0100

    feat(model): add BookDetailsResponse for individual book information

---
+++ src/main/java/com/pictet/adventurebook/model/dto/response/BookDetailsResponse.java
@@ -0,0 +1,20 @@
+package com.pictet.adventurebook.model.dto.response;
+
+import com.pictet.adventurebook.model.type.CategoryType;
+import com.pictet.adventurebook.model.type.DifficultyType;
+
+import java.util.List;
+import java.util.Set;
+
+public record BookDetailsResponse(
+        Long id,
+        String title,
+        String author,
+        DifficultyType difficulty,
+        Set<CategoryType> categories,
+        boolean valid,
+        List<String> validationErrors,
+        List<String> warnings
+) {
+
+}
