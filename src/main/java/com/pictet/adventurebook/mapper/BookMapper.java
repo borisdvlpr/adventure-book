b8888a88284138fc3e3a45d792b385f0a1957332
borisdvlpr <eduffmiranda@hotmail.com>
2026-09-02 17:48:43 +0100

    feat(mapper): add toBookDetailsResponde method in BookMapper

--- src/main/java/com/pictet/adventurebook/mapper/BookMapper.java
+++ src/main/java/com/pictet/adventurebook/mapper/BookMapper.java
@@ -1,9 +1,11 @@
 package com.pictet.adventurebook.mapper;
 
+import com.pictet.adventurebook.model.dto.response.BookDetailsResponse;
 import com.pictet.adventurebook.model.dto.response.BookSummaryResponse;
 import com.pictet.adventurebook.model.entity.Book;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
 import java.util.TreeSet;
 
 @Component
@@ -17,6 +19,19 @@ public class BookMapper {
                 book.getDifficulty(),
                 new TreeSet<>(book.getCategories()),
                 book.isValid()
+        );
+    }
+
+    public BookDetailsResponse toBookDetailsResponse(Book book) {
+        return new BookDetailsResponse(
+                book.getId(),
+                book.getTitle(),
+                book.getAuthor(),
+                book.getDifficulty(),
+                new TreeSet<>(book.getCategories()),
+                book.isValid(),
+                List.copyOf(book.getValidationErrors()),
+                List.copyOf(book.getWarnings())
         );
     }
 }
