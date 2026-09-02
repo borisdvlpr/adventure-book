be3fb010dff3047de6efc381214af7db11207bba
borisdvlpr <eduffmiranda@hotmail.com>
2026-09-02 19:32:48 +0100

    feat(service): add getBook method to BookService

--- src/main/java/com/pictet/adventurebook/service/BookService.java
+++ src/main/java/com/pictet/adventurebook/service/BookService.java
@@ -1,19 +1,19 @@
 package com.pictet.adventurebook.service;
 
+import com.pictet.adventurebook.exception.BookNotFoundException;
 import com.pictet.adventurebook.mapper.BookMapper;
 import com.pictet.adventurebook.model.dto.request.BookSearchCriteria;
+import com.pictet.adventurebook.model.dto.response.BookDetailsResponse;
 import com.pictet.adventurebook.model.dto.response.BookSummaryResponse;
 import com.pictet.adventurebook.model.dto.response.PageResponse;
 import com.pictet.adventurebook.model.entity.Book;
 import com.pictet.adventurebook.repository.BookRepository;
-
 import com.pictet.adventurebook.repository.BookSpecification;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
 
 @Service
 @RequiredArgsConstructor
@@ -27,5 +27,17 @@ public class BookService {
         Page<Book> page = bookRepository.findAll(BookSpecification.matching(criteria), pageable);
 
         return PageResponse.from(page, bookMapper::toBookSummaryResponse);
+    }
+
+    @Transactional(readOnly = true)
+    public BookDetailsResponse getBook(Long id) {
+        Book book = findBook(id);
+        return bookMapper.toBookDetailsResponse(book);
+    }
+
+    private Book findBook(Long id) {
+        return bookRepository.findById(id).orElseThrow(() ->
+                new BookNotFoundException(String.format("No book found with id '%d'.", id))
+        );
     }
 }
