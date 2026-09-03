d1c696e1d5a1fd67c64dd797d91ac6dad6f9cd2f
borisdvlpr <eduffmiranda@hotmail.com>
2026-09-03 10:26:55 +0100

    feat(controller): add endpoints to list and start games on game controller

--- src/main/java/com/pictet/adventurebook/controller/GameController.java
+++ src/main/java/com/pictet/adventurebook/controller/GameController.java
@@ -3,9 +3,15 @@ package com.pictet.adventurebook.controller;
 import com.pictet.adventurebook.model.dto.request.ChoiceRequest;
 import com.pictet.adventurebook.model.dto.request.StartGameRequest;
 import com.pictet.adventurebook.model.dto.response.GameStateResponse;
+import com.pictet.adventurebook.model.dto.response.GameSummaryResponse;
+import com.pictet.adventurebook.model.dto.response.PageResponse;
+import com.pictet.adventurebook.model.type.GameStatusType;
 import com.pictet.adventurebook.service.GameService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,15 +22,24 @@ public class GameController {
 
     private final GameService gameService;
 
-    @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public GameStateResponse startGame(@Valid @RequestBody StartGameRequest request) {
-        return gameService.startGame(request);
+    @GetMapping
+    public PageResponse<GameSummaryResponse> listGames(
+            @RequestParam String playerId,
+            @RequestParam(required = false)GameStatusType status,
+            @PageableDefault(size = 20, sort = "updatedAt", direction = Sort.Direction.DESC)Pageable pageable) {
+
+        return gameService.listGames(playerId, status, pageable);
     }
 
     @GetMapping("/{id}")
     public GameStateResponse getGame(@PathVariable Long id) {
         return gameService.getGame(id);
+    }
+
+    @PostMapping("/new")
+    @ResponseStatus(HttpStatus.CREATED)
+    public GameStateResponse startGame(@Valid @RequestBody StartGameRequest request) {
+        return gameService.startGame(request);
     }
 
     @PostMapping("/{id}/choices")
