d196393a4a139cebc6929ba15fcb9e1448826a46
borisdvlpr <eduffmiranda@hotmail.com>
2026-09-03 10:27:25 +0100

    feat(controller): add endpoint to create new book on book controller

--- src/main/java/com/pictet/adventurebook/controller/BookController.java
+++ src/main/java/com/pictet/adventurebook/controller/BookController.java
@@ -1,16 +1,22 @@
 package com.pictet.adventurebook.controller;
 
+import com.pictet.adventurebook.model.dto.BookImportDto;
 import com.pictet.adventurebook.model.dto.request.BookSearchCriteria;
 import com.pictet.adventurebook.model.dto.response.BookDetailsResponse;
 import com.pictet.adventurebook.model.dto.response.BookSummaryResponse;
 import com.pictet.adventurebook.model.dto.response.PageResponse;
 import com.pictet.adventurebook.model.type.CategoryType;
 import com.pictet.adventurebook.service.BookService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequiredArgsConstructor
@@ -31,6 +37,18 @@ public class BookController {
     // @Cacheable(cacheNames = CacheConfig.BOOKS_CACHE, key = "#id")
     public BookDetailsResponse getBook(@PathVariable Long id) {
         return bookService.getBook(id);
+    }
+
+    @PostMapping("/new")
+    public ResponseEntity<BookDetailsResponse> createBook(@Valid @RequestBody BookImportDto request) {
+        BookDetailsResponse newBook = bookService.createNewBook(request);
+
+        URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(newBook.id())
+                .toUri();
+
+        return ResponseEntity.created(location).body(newBook);
     }
 
     @PutMapping("/{id}/categories/{category}")
