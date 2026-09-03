122de8ab1ae6b9d9ea1a12459b831bd1c29decf0
borisdvlpr <eduffmiranda@hotmail.com>
2026-09-04 00:34:04 +0100

    feat(config): add redis cache configuration class and properties

--- src/main/java/com/pictet/adventurebook/config/CacheConfig.java
+++ src/main/java/com/pictet/adventurebook/config/CacheConfig.java
@@ -1,11 +1,39 @@
 package com.pictet.adventurebook.config;
 
+import com.pictet.adventurebook.model.dto.response.BookDetailsResponse;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext.SerializationPair;
 
-//@Configuration
-//@EnableCaching
+import java.time.Duration;
+
+@Configuration
+@EnableCaching
 public class CacheConfig {
 
     public static final String BOOKS_CACHE = "books";
+    private static final Duration BOOKS_TTL = Duration.ofHours(1);
+    private static final String KEY_PREFIX = "adventurebook:";
+
+    @Bean
+    public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
+        return RedisCacheManager.builder(connectionFactory)
+                .withCacheConfiguration(BOOKS_CACHE, typedCache(BookDetailsResponse.class, BOOKS_TTL))
+                .disableCreateOnMissingCache()
+                .transactionAware()
+                .build();
+    }
+
+    private static RedisCacheConfiguration typedCache(Class<?> type, Duration ttl) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(ttl)
+                .disableCachingNullValues()
+                .computePrefixWith(cacheName -> KEY_PREFIX + cacheName + "::")
+                .serializeValuesWith(SerializationPair.fromSerializer(new JacksonJsonRedisSerializer<>(type)));
+    }
 }
