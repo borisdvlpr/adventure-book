c2f9d70ef27f73a64cc464988df7ea8d1866bf57
borisdvlpr <eduffmiranda@hotmail.com>
2026-09-03 07:42:47 +0100

    feat(mapper): update toBookDetailsResponse method with initial section number and add toSectionResponse and toOptionResponse methods

--- src/main/java/com/pictet/adventurebook/mapper/BookMapper.java
+++ src/main/java/com/pictet/adventurebook/mapper/BookMapper.java
@@ -2,7 +2,10 @@ package com.pictet.adventurebook.mapper;
 
 import com.pictet.adventurebook.model.dto.response.BookDetailsResponse;
 import com.pictet.adventurebook.model.dto.response.BookSummaryResponse;
+import com.pictet.adventurebook.model.dto.response.SectionResponse;
 import com.pictet.adventurebook.model.entity.Book;
+import com.pictet.adventurebook.model.entity.Option;
+import com.pictet.adventurebook.model.entity.Section;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
@@ -22,7 +25,7 @@ public class BookMapper {
         );
     }
 
-    public BookDetailsResponse toBookDetailsResponse(Book book) {
+    public BookDetailsResponse toBookDetailsResponse(Book book, Integer beginSectionNumber) {
         return new BookDetailsResponse(
                 book.getId(),
                 book.getTitle(),
@@ -31,7 +34,26 @@ public class BookMapper {
                 new TreeSet<>(book.getCategories()),
                 book.isValid(),
                 List.copyOf(book.getValidationErrors()),
-                List.copyOf(book.getWarnings())
+                List.copyOf(book.getWarnings()),
+                beginSectionNumber
+        );
+    }
+
+    public SectionResponse toSectionResponse(Long bookId, Section section) {
+        return new SectionResponse(
+                bookId,
+                section.getSectionNumber(),
+                section.getText(),
+                section.getType(),
+                section.getOptions().stream().map(this::toOptionResponse).toList()
+        );
+    }
+
+    private SectionResponse.OptionResponse toOptionResponse(Option option) {
+        return new SectionResponse.OptionResponse(
+                option.getOrdinal(),
+                option.getDescription(),
+                option.getGotoNumber()
         );
     }
 }
