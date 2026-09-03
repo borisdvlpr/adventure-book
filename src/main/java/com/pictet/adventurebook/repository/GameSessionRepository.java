899f922d44cc5f5f92f88b6f3910020bbdea99a9
borisdvlpr <eduffmiranda@hotmail.com>
2026-09-03 10:18:06 +0100

    feat(repository): add findByPlayerId and findByPlayerIdAndStatus to GameSessionRepository

--- src/main/java/com/pictet/adventurebook/repository/GameSessionRepository.java
+++ src/main/java/com/pictet/adventurebook/repository/GameSessionRepository.java
@@ -1,10 +1,19 @@
 package com.pictet.adventurebook.repository;
 
 import com.pictet.adventurebook.model.entity.GameSession;
+import com.pictet.adventurebook.model.type.GameStatusType;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface GameSessionRepository extends JpaRepository<GameSession, Long> {
 
+    @EntityGraph(attributePaths = "book")
+    Page<GameSession> findByPlayerId(String playerId, Pageable pageable);
+
+    @EntityGraph(attributePaths = "book")
+    Page<GameSession> findByPlayerIdAndStatus(String playerId, GameStatusType status, Pageable pageable);
 }
