eff4a8f716d06107a61bd41223291959cef07d4f
borisdvlpr <eduffmiranda@hotmail.com>
2026-09-03 02:02:59 +0100

    refactor(exception): build message from book id in BookNotFoundException

--- src/main/java/com/pictet/adventurebook/exception/BookNotFoundException.java
+++ src/main/java/com/pictet/adventurebook/exception/BookNotFoundException.java
@@ -2,7 +2,7 @@ package com.pictet.adventurebook.exception;
 
 public class BookNotFoundException extends RuntimeException {
 
-    public BookNotFoundException(String message) {
-        super(message);
+    public BookNotFoundException(Long bookId) {
+        super(String.format("No book found with id '%d'.", bookId));
     }
 }
